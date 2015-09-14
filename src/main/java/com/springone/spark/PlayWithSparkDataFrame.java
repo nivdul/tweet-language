c7351aeb3743c7a23ef3a71728bbddbbb83dc42a
Ludwine Probst <ludwine.probst@gmail.com>
2015-09-14 07:58:52 -0400

    update dataframe

--- src/main/java/com/springone/spark/PlayWithSparkDataFrame.java
+++ src/main/java/com/springone/spark/PlayWithSparkDataFrame.java
@@ -1,5 +1,6 @@
 package com.springone.spark;
 
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
@@ -19,7 +20,8 @@ import org.apache.spark.sql.SQLContext;
  *  In here we create a dataframe with the content of a JSON file.
  *
  */
-public class PlayWithSparkStreaming {
+public class PlayWithSparkDataFrame {
+  final static Logger log = Logger.getLogger(PlayWithSparkDataFrame.class);
 
   // WARNING: Change the path
   // the path file where we stored the data
@@ -27,7 +29,7 @@ public class PlayWithSparkStreaming {
 
   public static void main(String[] args) {
     SparkConf conf = new SparkConf()
-        .setAppName("Load data")
+        .setAppName("Spark DataFrames")
         .setMaster("local[*]");
 
     JavaSparkContext sc = new JavaSparkContext(conf);
@@ -37,21 +39,25 @@ public class PlayWithSparkStreaming {
     SQLContext sqlContext = new SQLContext(sc);
 
     // load the data (json file here) and register the data in the "tweets" table.
-    DataFrame tweets = sqlContext.jsonFile(pathToFile);
-    tweets.registerTempTable("tweets");
+    DataFrame df = sqlContext.jsonFile(pathToFile);
+    df.registerTempTable("tweets");
 
     // Displays the content of the DataFrame to stdout
-    tweets.show();
+    //tweets.show();
+
     // we see something like that:
     // id         lang        name                 text
     // 632952234  en          Remembrance Day      Air Force Upgrade...
 
-    // filter tweets in japanese
-    DataFrame filtered = tweets.filter(tweets.col("lang").equalTo("ja")).toDF();
+    // filter tweets in english and french
+    DataFrame filtered = df.filter((df.col("lang").equalTo("en"))
+                                        .or(df.col("lang").equalTo("fr")))
+                           .toDF();
+    
     filtered.show();
-    // we see something like that:
+    // you should see something like that:
     // id         lang        name                 text
-    // 2907381456 ja          なっちゃん            クナイ投げた方がいいか　投げまくるか
+    // 2907381456 fr          sophie               springOne ça commence !
 
     // Count the tweets for each language
     // It will be use to determine the number of clusters we want for the K-means algorithm.
@@ -60,6 +66,8 @@ public class PlayWithSparkStreaming {
     System.out.println("number of languages: " + result.collectAsList().size());
     System.out.println(result.collectAsList());
 
+    // another way
+    df.groupBy("lang").count().show();
   }
 
 }
